7c5f328d9d49bdcc8bfc79805bb09095f36e1bfe
Amritpalsingh0007 <amritpalsingh220923@gmail.com>
2024-07-30 18:43:41 +0530

    Menu ui commite: created a background and one image and three buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/pages/friend_vs_friend.dart';
-// import 'package:tictactoe/pages/menu.dart';
 
 
 void main(){
@@ -10,11 +9,3 @@ void main(){
     ),
   );
 }
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp();
-//   }
-// }
